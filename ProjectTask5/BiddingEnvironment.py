fix reward formula

ProjectTask5/BiddingEnvironment.py
@@ -14,28 +14,33 @@ class BiddingEnvironment():
         self.mu_new = mu_new
         self.sigma_new = sigma_new
         self.total_returns_per_arm = [[] for _ in range(n_arms)]
+        #self.results = ""
 
     def round(self,pulled_arm):
-        delta_customers = self.bid_modifiers[pulled_arm]**2
-        new_customers = round(np.random.normal((self.mu_new + 5*delta_customers),self.sigma_new))
+        delta_customers = 5*(self.bid_modifiers[pulled_arm]*2)
+        new_customers = round(np.random.normal(round(self.mu_new + delta_customers),self.sigma_new))
         single_rewards = np.zeros(new_customers)
         single_cost_per_click = np.zeros(new_customers)
 
         for i in range (0, new_customers):
             customer = Customer(self.conv_rate)
             single_rewards[i] = customer.round_costumer()
-            single_cost_per_click[i] = self.bids[pulled_arm] - abs(np.random.normal(0, self.bids[pulled_arm]/10))
+            single_cost_per_click[i] = self.bids[pulled_arm] - abs(np.random.normal(self.bids[pulled_arm], 0.1))/10
 
         sells = np.sum(single_rewards)
         n_returns = np.zeros(int(sells))
 
         for i in range(0,int(sells)):
-            n_returns[i] = abs(round(np.random.normal(15 - 2*self.price,3)))
+            n_returns[i] = max(0, (round(np.random.normal(15 - 2*self.price,2))))
+            #n_returns[i] = 15-2*self.price
 
         costs = np.sum(single_cost_per_click)
         total_returns = np.sum(n_returns)
         self.total_returns_per_arm[pulled_arm] = np.append(self.total_returns_per_arm[pulled_arm],total_returns)
         money_reward = ((sells + total_returns) * self.price) - costs
+        
+        #self.results = "arm: {}, sales: {}, total_returns: {}, costs: {}, total:{}".format(pulled_arm, sells, total_returns, costs, money_reward)
+
         return money_reward
 
 class Customer():

ProjectTask5/experiment.py
@@ -11,31 +11,28 @@ from GaussianTS_Learner import *
 n_arms = 10
 
 price = 5.5 #fixed
-conv_rate = 0.2 #prob of buying after click, known
+conv_rate = 0.5 #prob of buying after click, known
 T = 365
 n_experiment = 100
 delay = 30
 mu_new_customer = 12
 sigma_new_customer = math.sqrt(4)
 
-#p = np.array([]) #p
 #bids = np.array([0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6])
 bids = np.array([0.5,0.7,0.9,1.1,1.4,1.5,1.7,1.9,2.1,2.3])
 
-bid_modifiers = np.array([0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0])
+bid_modifiers = np.array([0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,1.8])
 
+opt_bid = bids[8]
 
-opt_bid = bids[5]
+delta_customers = 5*(bid_modifiers[8]*2)
 
-delta_customers = 5*(bid_modifiers[5]**2)
-
-opt = (conv_rate*price*(mu_new_customer + delta_customers)* ((15-2*price)+1)) - (opt_bid - opt_bid/10) * (mu_new_customer + delta_customers)
+opt = conv_rate*(round(mu_new_customer + delta_customers))*price*(15 - 2*price + 1) - (opt_bid - opt_bid/10) * round(mu_new_customer + delta_customers)
 
 
 ts_rewards_per_experiment = []
 ucb_rewards_per_experiment = []
 
-medie_di_medie = []
 
 pulled_arm_buffer_ts = Queue(maxsize=31)
 pulled_arm_buffer_ucb = Queue(maxsize=31)
@@ -59,12 +56,11 @@ for e in range(0,n_experiment):
             gts_learner.update(after_30_days_arm_ts,rewards)
 
     ts_rewards_per_experiment.append(gts_learner.collected_rewards)
-    
-    medie_di_medie = np.append(medie_di_medie,np.mean(env.total_returns_per_arm[0]))
+    #print(env.results)
     print(e)
 
 
-print(np.mean(medie_di_medie))
+print("optimal= {}".format(opt))
 plt.figure(0)
 plt.xlabel("t")
 plt.ylabel("Regret")
@@ -74,7 +70,7 @@ plt.show()
 
 
 
-x=np.arange(-100,100,0.01)
+x=np.arange(-100,600,0.01)
 plt.plot(x, norm.pdf(x, gts_learner.means_of_rewards[0], 1/gts_learner.precision_of_rewards[0]), label='0')
 plt.plot(x, norm.pdf(x, gts_learner.means_of_rewards[1], 1/gts_learner.precision_of_rewards[1]), label='1')
 plt.plot(x, norm.pdf(x, gts_learner.means_of_rewards[2], 1/gts_learner.precision_of_rewards[2]), label='2')
@@ -86,7 +82,8 @@ plt.plot(x, norm.pdf(x, gts_learner.means_of_rewards[7], 1/gts_learner.precision
 plt.plot(x, norm.pdf(x, gts_learner.means_of_rewards[8], 1/gts_learner.precision_of_rewards[8]), label='8')
 plt.plot(x, norm.pdf(x, gts_learner.means_of_rewards[9], 1/gts_learner.precision_of_rewards[9]), label='9')
 
-for i in range(len(gts_learner.means_of_rewards)):
-    print("mean: {}, cdf in 0: {}".format(gts_learner.means_of_rewards[i],norm.cdf(0, gts_learner.means_of_rewards[i], 1/gts_learner.precision_of_rewards[i])))
+#for i in range(len(gts_learner.means_of_rewards)):
+    #print("mean: {}, cdf in 0: {}".format(gts_learner.means_of_rewards[i],norm.cdf(0, gts_learner.means_of_rewards[i], 1/gts_learner.precision_of_rewards[i])))
+
 plt.legend()
 plt.show()