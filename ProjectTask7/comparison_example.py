Fix variance in Task 3

ProjectTask7/comparison_example.py
@@ -23,8 +23,8 @@ prices = np.array(UtilFunctions.global_prices)
 bids = np.array(UtilFunctions.global_bids)
 prod_cost = 3.0
 n_arms = 10
-T = 150
-n_experiment = 1
+T = 365
+n_experiment = 30
 delay = 30
 
 contexts_prob = np.array([  np.array([np.array([conv_c1(x) for x in prices]),